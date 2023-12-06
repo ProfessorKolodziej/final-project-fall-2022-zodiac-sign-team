152c6560c40b35d80d4199f4597cd0a662623f94
SerenaZhu0823 <zyn@bu.edu>
2023-12-06 18:27:07 -0500

    1

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,3 +5,28 @@
 // - Do not use onclick - use addEventListener instead
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
+
+
+
+var check = document.querySelectorAll(".good input")
+var btn = document.querySelector("button")
+var a = document.querySelector("a")
+console.log(check)
+var count = 0
+
+btn.addEventListener('click', function () {
+    let count = 0; 
+    for (let i = 0; i < check.length; i++) {
+        if (check[i].checked) {
+            count++;
+        }
+    }
+    if (count === 0) {
+        alert("If you don't have an idea, just look at the answer");
+        console.log(count);
+        
+    } else {
+        alert("Check your answers!");
+    }
+});
+
